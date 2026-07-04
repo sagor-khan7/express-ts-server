c323d99c14e94cb69a8410224567b9cd730d16c1
sagor khan <sksagor791@gmail.com>
2026-07-04 12:16:46 +0600

    created config file to make envcongif and created db.ts to separate db logic

---
+++ src/config/db.ts
@@ -0,0 +1,44 @@
+import { Pool } from "pg";
+import config from ".";
+
+export const pool = new Pool({
+  connectionString: `${config.connection_str}`,
+});
+
+const initDB = async () => {
+  try {
+    // 1. Create Users Table
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone VARCHAR(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+
+    // 2. Create Todos Table
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(200) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+      );
+    `);
+
+    console.log("Database tables initialized successfully!");
+  } catch (error) {
+    console.error("Error initializing database tables:", error);
+  }
+};
+
+export default initDB;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import config from "./config";
+import initDB, { pool } from "./config/db";
 
 const app = express();
-const port = 5000;
+const port = config.port;
 app.use(express.json());
-
-const pool = new Pool({
-  connectionString: `${process.env.CONNECTION_STR}`,
-});
 
 // middlewares
 const logger = (req: Request, res: Response, next: NextFunction) => {
@@ -23,42 +17,7 @@ app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-const initDB = async () => {
-  try {
-    // 1. Create Users Table
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-      );
-    `);
-
-    // 2. Create Todos Table
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS todos (
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-      );
-    `);
-
-    console.log("Database tables initialized successfully!");
-  } catch (error) {
-    console.error("Error initializing database tables:", error);
-  }
-};
-
+// initializing db
 initDB();
 
 // users CRUD
@@ -298,8 +257,6 @@ app.delete("/todos/:id", async (req: Request, res: Response) => {
     });
   }
 });
-
-
 
 app.use((req, res) => {
   res.status(404).json({
