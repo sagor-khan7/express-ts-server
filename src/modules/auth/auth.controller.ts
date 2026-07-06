2e54da42ca0f5b22f54f76c4b247a985a26e38bb
sagor khan <sksagor791@gmail.com>
2026-07-06 13:33:59 +0600

    installed jwt to create token, auth routes, controller and services are created and created login functionality for user, and upon login a token was assigned to the user

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { authServices } from "./auth.service";
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const result = await authServices.loginUser(email, password);
+    console.log(result);
+    res.status(200).json({
+      success: true,
+      message: "Login successfully",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error,
+    });
+  }
+};
+
+export const authControllers = {
+  loginUser,
+};
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import initDB, { pool } from "./config/db";
 import logger from "./middleware/logger";
 import { userRoutes } from "./modules/user/user.routes";
 import { todosRouter } from "./modules/todo/todo.routes";
+import { authRoutes } from "./modules/auth/auth.routes";
 
 const app = express();
 const port = config.port;
@@ -23,6 +24,9 @@ app.use("/users", userRoutes);
 
 // todos CRUD
 app.use("/todos", todosRouter);
+
+// auth routes
+app.use("/auth", authRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
